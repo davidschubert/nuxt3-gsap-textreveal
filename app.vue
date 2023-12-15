45255ba1da6bec15f98d20752f463c394484c9fb
David Schubert <github@davidschubert.com>
2023-12-15 02:33:53 -1000

    update smooth touch to false

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ onNuxtReady(async () => {
     const smoother = ScrollSmoother.create({
         smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
         effects: false, // looks for data-speed and data-lag attributes on elements
-        smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+        smoothTouch: false, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
     });
 });
 
